5c85bc5e401f11344ffdb8a38e3bd059a1a71f5f
anna <annamikhailenko@gmail.com>
2026-09-01 18:30:13 +0200

    Added Observable to the service + unit tests

--- src/app/core/services/swapi.service.spec.ts
+++ src/app/core/services/swapi.service.spec.ts
@@ -4,8 +4,8 @@ import {
   HttpTestingController,
   provideHttpClientTesting,
 } from '@angular/common/http/testing';
-import { SwapiService } from './swapi.service';
 
+import { SwapiService } from './swapi.service';
 import { environment } from '../../environment';
 
 describe('SwapiService', () => {
@@ -29,91 +29,191 @@ describe('SwapiService', () => {
     httpMock.verify();
   });
 
-  it('should create', () => {
+  it('should be created', () => {
     expect(service).toBeTruthy();
   });
 
   it('should get person', () => {
-    service.getPerson('1').subscribe();
-    httpMock
-      .expectOne(`${environment.apiUrl}/people/1`)
-      .flush({});
+    const mockPerson = {} as any;
+
+    service.getPerson('1').subscribe((person) => {
+      expect(person).toEqual(mockPerson);
+    });
+
+    const req = httpMock.expectOne(
+      `${environment.apiUrl}/people/1`
+    );
+
+    expect(req.request.method).toBe('GET');
+
+    req.flush(mockPerson);
   });
 
   it('should get planet', () => {
-    service.getPlanet('1').subscribe();
-    httpMock
-      .expectOne(`${environment.apiUrl}/planets/1`)
-      .flush({});
+    const mockPlanet = {} as any;
+
+    service.getPlanet('1').subscribe((planet) => {
+      expect(planet).toEqual(mockPlanet);
+    });
+
+    const req = httpMock.expectOne(
+      `${environment.apiUrl}/planets/1`
+    );
+
+    expect(req.request.method).toBe('GET');
+
+    req.flush(mockPlanet);
   });
 
   it('should get specie', () => {
-    service.getSpecie('1').subscribe();
-    httpMock
-      .expectOne(`${environment.apiUrl}/species/1`)
-      .flush({});
+    const mockSpecie = {} as any;
+
+    service.getSpecie('1').subscribe((specie) => {
+      expect(specie).toEqual(mockSpecie);
+    });
+
+    const req = httpMock.expectOne(
+      `${environment.apiUrl}/species/1`
+    );
+
+    expect(req.request.method).toBe('GET');
+
+    req.flush(mockSpecie);
   });
 
   it('should get vehicle', () => {
-    service.getVehicle('1').subscribe();
-    httpMock
-      .expectOne(`${environment.apiUrl}/vehicles/1`)
-      .flush({});
+    const mockVehicle = {} as any;
+
+    service.getVehicle('1').subscribe((vehicle) => {
+      expect(vehicle).toEqual(mockVehicle);
+    });
+
+    const req = httpMock.expectOne(
+      `${environment.apiUrl}/vehicles/1`
+    );
+
+    expect(req.request.method).toBe('GET');
+
+    req.flush(mockVehicle);
   });
 
   it('should get starship', () => {
-    service.getStarship('1').subscribe();
-    httpMock
-      .expectOne(`${environment.apiUrl}/starship/1`)
-      .flush({});
+    const mockStarship = {} as any;
+
+    service.getStarship('1').subscribe((starship) => {
+      expect(starship).toEqual(mockStarship);
+    });
+
+    const req = httpMock.expectOne(
+      `${environment.apiUrl}/starships/1`
+    );
+
+    expect(req.request.method).toBe('GET');
+
+    req.flush(mockStarship);
   });
 
   it('should get people', () => {
-    service.getPeople().subscribe();
-    httpMock
-      .expectOne(`${environment.apiUrl}/people`)
-      .flush([]);
+    const mockPeople = [{}] as any[];
+
+    service.getPeople().subscribe((people) => {
+      expect(people).toEqual(mockPeople);
+    });
+
+    const req = httpMock.expectOne(
+      `${environment.apiUrl}/people`
+    );
+
+    expect(req.request.method).toBe('GET');
+
+    req.flush(mockPeople);
   });
 
   it('should get films', () => {
-    service.getFilms().subscribe();
-    httpMock
-      .expectOne(`${environment.apiUrl}/films`)
-      .flush([]);
+    const mockFilms = [{}] as any[];
+
+    service.getFilms().subscribe((films) => {
+      expect(films).toEqual(mockFilms);
+    });
+
+    const req = httpMock.expectOne(
+      `${environment.apiUrl}/films`
+    );
+
+    expect(req.request.method).toBe('GET');
+
+    req.flush(mockFilms);
   });
 
   it('should get planets', () => {
-    service.getPlanets().subscribe();
-    httpMock
-      .expectOne(`${environment.apiUrl}/planets`)
-      .flush([]);
+    const mockPlanets = [{}] as any[];
+
+    service.getPlanets().subscribe((planets) => {
+      expect(planets).toEqual(mockPlanets);
+    });
+
+    const req = httpMock.expectOne(
+      `${environment.apiUrl}/planets`
+    );
+
+    expect(req.request.method).toBe('GET');
+
+    req.flush(mockPlanets);
   });
 
   it('should get species', () => {
-    service.getSpecies().subscribe();
-    httpMock
-      .expectOne(`${environment.apiUrl}/species`)
-      .flush([]);
+    const mockSpecies = [{}] as any[];
+
+    service.getSpecies().subscribe((species) => {
+      expect(species).toEqual(mockSpecies);
+    });
+
+    const req = httpMock.expectOne(
+      `${environment.apiUrl}/species`
+    );
+
+    expect(req.request.method).toBe('GET');
+
+    req.flush(mockSpecies);
   });
 
   it('should get vehicles', () => {
-    service.getVehicles().subscribe();
-    httpMock
-      .expectOne(`${environment.apiUrl}/vehicles`)
-      .flush([]);
+    const mockVehicles = [{}] as any[];
+
+    service.getVehicles().subscribe((vehicles) => {
+      expect(vehicles).toEqual(mockVehicles);
+    });
+
+    const req = httpMock.expectOne(
+      `${environment.apiUrl}/vehicles`
+    );
+
+    expect(req.request.method).toBe('GET');
+
+    req.flush(mockVehicles);
   });
 
   it('should get starships', () => {
-    service.getStarships().subscribe();
-    httpMock
-      .expectOne(`${environment.apiUrl}/starships`)
-      .flush([]);
+    const mockStarships = [{}] as any[];
+
+    service.getStarships().subscribe((starships) => {
+      expect(starships).toEqual(mockStarships);
+    });
+
+    const req = httpMock.expectOne(
+      `${environment.apiUrl}/starships`
+    );
+
+    expect(req.request.method).toBe('GET');
+
+    req.flush(mockStarships);
   });
 
-  it('should handle api error', () => {
+  it('should handle server errors', () => {
     service.getFilms().subscribe({
-      next: () => fail('should error'),
-      error: (error) => {
+      next: () => fail('Expected request to fail'),
+      error: (error: Error) => {
+        expect(error).toBeTruthy();
         expect(error.message).toContain('Error 500');
       },
     });
@@ -128,7 +228,3 @@ describe('SwapiService', () => {
     });
   });
 });
-
-function fail(arg0: string): void {
-  throw new Error('Function not implemented.');
-}
--- src/app/core/services/swapi.service.ts
+++ src/app/core/services/swapi.service.ts
@@ -1,7 +1,7 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { environment } from '../../environment';
-import { catchError, throwError } from 'rxjs';
+import { catchError, Observable, throwError } from 'rxjs';
 import { Planet, Film, Person, Specie, Vehicle, Starship } from '../models';
 
 @Injectable({
@@ -23,70 +23,69 @@ export class SwapiService {
 
     return throwError(() => new Error(errorMessage));
   }
+getPerson(id: string): Observable<Person> {
+  return this.http
+    .get<Person>(`${environment.apiUrl}/people/${id}`)
+    .pipe(catchError(this.handleError));
+}
 
-  getPerson(id: string) {
-    return this.http
-      .get(`${environment.apiUrl}/people/${id}`)
-      .pipe(catchError(this.handleError));
-  }
+getPlanet(id: string): Observable<Planet> {
+  return this.http
+    .get<Planet>(`${environment.apiUrl}/planets/${id}`)
+    .pipe(catchError(this.handleError));
+}
 
-  getPlanet(id: string) {
-    return this.http
-      .get(`${environment.apiUrl}/planets/${id}`)
-      .pipe(catchError(this.handleError));
-  }
+getSpecie(id: string): Observable<Specie> {
+  return this.http
+    .get<Specie>(`${environment.apiUrl}/species/${id}`)
+    .pipe(catchError(this.handleError));
+}
 
-  getSpecie(id: string) {
-    return this.http
-      .get(`${environment.apiUrl}/species/${id}`)
-      .pipe(catchError(this.handleError));
-  }
+getVehicle(id: string): Observable<Vehicle> {
+  return this.http
+    .get<Vehicle>(`${environment.apiUrl}/vehicles/${id}`)
+    .pipe(catchError(this.handleError));
+}
 
-  getVehicle(id: string) {
-    return this.http
-      .get(`${environment.apiUrl}/vehicles/${id}`)
-      .pipe(catchError(this.handleError));
-  }
+getStarship(id: string): Observable<Starship> {
+  return this.http
+    .get<Starship>(`${environment.apiUrl}/starships/${id}`)
+    .pipe(catchError(this.handleError));
+}
 
-  getStarship(id: string) {
-    return this.http
-      .get(`${environment.apiUrl}/starship/${id}`)
-      .pipe(catchError(this.handleError));
-  }
+getPeople(): Observable<Person[]> {
+  return this.http
+    .get<Person[]>(`${environment.apiUrl}/people`)
+    .pipe(catchError(this.handleError));
+}
 
-  getPeople() {
-    return this.http
-      .get<Person[]>(`${environment.apiUrl}/people`)
-      .pipe(catchError(this.handleError));
-  }
+getFilms(): Observable<Film[]> {
+  return this.http
+    .get<Film[]>(`${environment.apiUrl}/films`)
+    .pipe(catchError(this.handleError));
+}
 
-  getFilms() {
-    return this.http
-      .get<Film[]>(`${environment.apiUrl}/films`)
-      .pipe(catchError(this.handleError));
-  }
+getPlanets(): Observable<Planet[]> {
+  return this.http
+    .get<Planet[]>(`${environment.apiUrl}/planets`)
+    .pipe(catchError(this.handleError));
+}
 
-  getPlanets() {
-    return this.http
-      .get<Planet[]>(`${environment.apiUrl}/planets`)
-      .pipe(catchError(this.handleError));
-  }
+getSpecies(): Observable<Specie[]> {
+  return this.http
+    .get<Specie[]>(`${environment.apiUrl}/species`)
+    .pipe(catchError(this.handleError));
+}
 
-  getSpecies() {
-    return this.http
-      .get<Specie[]>(`${environment.apiUrl}/species`)
-      .pipe(catchError(this.handleError));
-  }
+getVehicles(): Observable<Vehicle[]> {
+  return this.http
+    .get<Vehicle[]>(`${environment.apiUrl}/vehicles`)
+    .pipe(catchError(this.handleError));
+}
 
-  getVehicles() {
-    return this.http
-      .get<Vehicle[]>(`${environment.apiUrl}/vehicles`)
-      .pipe(catchError(this.handleError));
-  }
-
-  getStarships() {
-    return this.http
-      .get<Starship[]>(`${environment.apiUrl}/starships`)
-      .pipe(catchError(this.handleError));
-  }
+getStarships(): Observable<Starship[]> {
+  return this.http
+    .get<Starship[]>(`${environment.apiUrl}/starships`)
+    .pipe(catchError(this.handleError));
+}
 }
